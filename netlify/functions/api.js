fa16796f4aaed0b57d07bdb503c9649ee27b889e
barry mcclain <barrymcclainrx@gmail.com>
2024-07-28 21:07:46 -0500

    Implement API hello world works, but not api

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,5 +1,4 @@
 exports.handler = async (event, context) => {
-    if (event.httpMethod === 'GET') {
         try {
             // Process the GET request as needed
             const data = require('../../rxlaws.json');
@@ -21,30 +20,6 @@ exports.handler = async (event, context) => {
                 body: JSON.stringify({ error: 'Failed to process GET request' }),
             };
         }
-    }
 
-};
 
-exports.handler = async (event, context) => {
-    if (event.httpMethod === 'POST') {
-        try {
-            // Parse the incoming JSON payload from the request body
-            const requestBody = JSON.parse(event.body);
-
-            // Save the data to a database or perform other necessary operations
-            // ...
-
-            // Return a success response
-            return {
-                statusCode: 200,
-                body: JSON.stringify({ message: 'POST request processed successfully' }),
-            };
-        } catch (error) {
-            // Return an error response if there was an issue processing the request
-            return {
-                statusCode: 400,
-                body: JSON.stringify({ error: 'Failed to process POST request' }),
-            };
-        }
-    }
 };
