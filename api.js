71413cf3ebff7f60c73dc5d5cbe2ca1b677154e8
barry mcclain <barrymcclainrx@gmail.com>
2025-02-16 18:07:58 -0600

    update to ADE

--- api.js
+++ api.js
@@ -11,6 +11,7 @@ let genericDrugName = [];
 let establishedPharmClass = [];
 let chemicalStructureClass = [];
 let reaction = [];
+let indication = [];
 let country = [];
 let byCount = null;
 let limit = null;
@@ -18,6 +19,12 @@ let safetyReportId = null;
 let url = "https://api.fda.gov/drug/event.json?";
 let isConstructingUrl = false;
 let isSearchInProgress = false;
+let sex = null;
+let ageGroup = null;
+let ageBegin = null;
+let ageEnd = null;
+let serious = null;
+let reportBy = "";
 
 const debounce = (func, delay) => {
     let timeoutId;
@@ -27,13 +34,19 @@ const debounce = (func, delay) => {
     };
 };
 
+function toDateInputValue(dateObject){
+    const local = new Date(dateObject);
+    local.setMinutes(dateObject.getMinutes() - dateObject.getTimezoneOffset());
+    return local.toJSON().slice(0,10);
+};
+
 function handleSearch(container, initialApiUrl) { // Store initial URL
      apiKey =  apiCall();
     constructUrl();
     const searchBar = container.querySelector('.search-bar');
     const charactersList = container.querySelector('.characters-list');
     const searchResults = container.querySelector('.search-results');
-
+    document.getElementById('endDate').value = toDateInputValue(new Date());
 
     container.addEventListener('click', async (event) => {
         const target = event.target;
@@ -67,6 +80,12 @@ function handleSearch(container, initialApiUrl) { // Store initial URL
                  byCount = null;
                  limit = null;
                  safetyReportId = null;
+                 ageEnd= null;
+                 ageBegin = null;
+                 ageGroup = null;
+                 sex = null;
+                 serious = null;
+                 reportBy ="";
                  url = "https://api.fda.gov/drug/event.json?";
                 constructUrl();
                 document.getElementById('showError').innerHTML = `<p> </p>`;
@@ -77,8 +96,10 @@ function handleSearch(container, initialApiUrl) { // Store initial URL
                     alert("Y axis must count by a variable, please select an option");
                 }
                 else {
+                    const showFinalCodeDiv = document.getElementById('showCode');
+
                 document.getElementById('showError').innerHTML = `<p> </p>`;
-                await fetchDataAndPlot(url, encodedApiKey);}
+                await fetchDataAndPlot(showFinalCodeDiv.innerText, encodedApiKey);}
 
             }
             else if (target.id === 'includeGenericName') {
@@ -103,12 +124,48 @@ function handleSearch(container, initialApiUrl) { // Store initial URL
                 constructUrl();
             } else if (target.id === 'includeLimit') {
                 limit = getSelectedValueForLimit();
+
+                if(limit <1 || limit > 1000){
+                    alert("Incorrect limit number");
+                    limit = null;
+                }
+                    constructUrl();
+
+            } else if (target.id === 'includeSex') {
+            sex = getSelectedValueForSex();
+            constructUrl();
+
+        } else if (target.id === 'includeAgeGroup') {
+            ageGroup = getSelectedValueForAgeGroup();
+            constructUrl();
+        }
+
+        else if (target.classList.contains('byCount')) {
+                byCount = target.name;
                 constructUrl();
 
+        } else if (target.id === 'includeIndication') {
+            const selectedIndication = getSelectedValueForIndication();
+            indication.push(selectedIndication);
+            constructUrl();
+        }
+         else if (target.id === 'addAgeRange') {
+                ageBegin = getSelectedValueForAgeBegin()
+                ageEnd = getSelectedValueForAgeEnd()
+                if ((ageBegin == null || ageEnd == null) || (ageBegin > ageEnd)) {
+                    alert("Missing Correct Age Range");
+                    ageBegin = "";
+                    ageEnd = "";
+                } else {
+                    constructUrl();
+                }
             }
-
-            else if (target.classList.contains('byCount')) {
-                byCount = target.name;
+            else if (target.id === 'includeSerious') {
+                serious = getSelectedValueForSerious();
+                constructUrl();
+            }
+            else if (target.id === 'includeReportBy') {
+                reportBy = getSelectedValueForReportBy();
                 constructUrl();
             }
 
@@ -178,7 +235,11 @@ function handleSearch(container, initialApiUrl) { // Store initial URL
                 }
                 else if (apiUrl.includes('patient.reaction.reactionmeddrapt.exact')) {
                     results = processReactionData(data.results);
-                } else {
+                }
+                else if (apiUrl.includes('patient.drug.drugindication.exact')) {
+                    results = processIndicationData(data.results);
+                }
+                else {
                     console.warn("Unknown API endpoint:", apiUrl);
                     results = [];
                 }
@@ -210,6 +271,75 @@ function handleSearch(container, initialApiUrl) { // Store initial URL
             searchResults.style.display = 'none'; // Hide the results after selection
         }
     });
+}
+
+function getSelectedValueForSex(){
+    const radioButtons = document.getElementsByName("sex");
+    let selectedValue = null;
+
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            selectedValue = radioButton.value;
+            break;
+        }
+    }
+
+    return selectedValue;
+}
+
+function getSelectedValueForIndication(){
+    const indicationInput = document.getElementById('indication');
+    return indicationInput.value;
+}
+
+function getSelectedValueForAgeGroup(){
+    const radioButtons = document.getElementsByName("ageGroup");
+    let selectedValue = null;
+
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            selectedValue = radioButton.value;
+            break;
+        }
+    }
+
+    return selectedValue;
+}
+
+function getSelectedValueForAgeBegin() {
+    const ageBeginInput = document.getElementById('ageBegin');
+    return ageBeginInput.value;
+}
+function getSelectedValueForAgeEnd() {
+    const ageEndInput = document.getElementById('ageEnd');
+    return ageEndInput.value;
+}
+function getSelectedValueForSerious(){
+    const radioButtons = document.getElementsByName("serious");
+    let selectedValue = null;
+
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            selectedValue = radioButton.value;
+            break;
+        }
+    }
+
+    return selectedValue;
+}
+
+function getSelectedValueForReportBy(){
+    const radioButtons = document.getElementsByName("reportBy");
+    let selectedValue = null;
+
+    for (const radioButton of radioButtons) {
+        if (radioButton.checked) {
+            selectedValue = radioButton.value;
+            break;
+        }
+    }
+
+    return selectedValue;
 }
 
 function getSelectedValueForGenericDrug() {
@@ -253,12 +383,19 @@ function constructUrl() {
 
     if (
         receivedDate ||
+        sex != null ||
+        ageGroup != null ||
         endDate ||
         genericDrugName.length > 0 ||
         establishedPharmClass.length > 0 ||
         chemicalStructureClass.length > 0 ||
         reaction.length > 0 ||
-        country.length > 0
+        country.length > 0 ||
+        indication.length > 0 ||
+        ageBegin != null ||
+        ageEnd != null ||
+        serious != null ||
+        reportBy
     ) {
         url += "search=";
     }
@@ -367,6 +504,109 @@ function constructUrl() {
         firstParamAdded = true;
     }
 
+    if (
+        (receivedDate && sex != null)||
+        ( genericDrugName.length > 0 && sex != null) ||
+        (establishedPharmClass.length > 0 && sex != null) ||
+        (chemicalStructureClass.length > 0 && sex != null) ||
+        (chemicalStructureClass.length > 0 && sex != null) ||
+        (country.length > 0 && sex != null)
+    ) {
+        url += "+AND+";
+    }
+
+    if (sex != null){url += `patient.patientsex:${sex}`;}
+
+    if (
+        (receivedDate && ageGroup != null)||
+        ( genericDrugName.length > 0 && ageGroup != null) ||
+        (establishedPharmClass.length > 0 && ageGroup != null) ||
+        (chemicalStructureClass.length > 0 && ageGroup != null) ||
+        (chemicalStructureClass.length > 0 && ageGroup != null) ||
+        (country.length > 0 && ageGroup != null) ||
+        (sex != null && ageGroup != null)
+    ) {
+        url += "+AND+";
+    }
+
+    if (ageGroup != null){url += `patient.patientagegroup:${ageGroup}`;}
+
+
+    if (
+        (receivedDate && indication.length > 0)||
+        ( genericDrugName.length > 0 && indication.length > 0) ||
+        (establishedPharmClass.length > 0 && indication.length > 0) ||
+        (chemicalStructureClass.length > 0 && indication.length > 0) ||
+        (chemicalStructureClass.length > 0 && indication.length > 0) ||
+        (country.length > 0 && indication.length > 0) ||
+        (sex != null && indication.length > 0) ||
+        (ageGroup != null && indication.length > 0)
+
+    ) {
+        url += "+AND+";
+    }
+
+    firstParamAdded = false;
+    for (const id of indication) {
+        if (firstParamAdded) {
+            url += `+"${id}"`;
+        }
+        if(firstParamAdded === false){
+            url += `patient.drug.drugindication.exact:"${id}"`;}
+        firstParamAdded = true;
+    }
+
+
+    if (
+        (receivedDate && ageBegin != null)||
+        ( genericDrugName.length > 0 && ageBegin != null) ||
+        (establishedPharmClass.length > 0 && ageBegin != null) ||
+        (chemicalStructureClass.length > 0 && ageBegin != null) ||
+        (chemicalStructureClass.length > 0 && ageBegin != null) ||
+        (country.length > 0 && ageBegin != null) ||
+        (sex != null && ageBegin != null) ||
+        (ageGroup != null && ageBegin != null) ||
+        (indication.length > 0 && ageBegin != null)
+    ) {
+        url += "+AND+";
+    }
+
+    if (ageBegin != null && ageEnd != null) {url += `(patient.patientonsetage:[${ageBegin}+TO+${ageEnd}])`;}
+
+    if (
+        (receivedDate && serious != null)||
+        ( genericDrugName.length > 0 && serious != null) ||
+        (establishedPharmClass.length > 0 && serious != null) ||
+        (chemicalStructureClass.length > 0 && serious != null) ||
+        (chemicalStructureClass.length > 0 && serious != null) ||
+        (country.length > 0 && serious != null) ||
+        (sex != null && serious != null) ||
+        (ageGroup != null && serious != null) ||
+        (indication.length > 0 && serious != null) ||
+        (ageBegin != null && serious != null)
+    ) {
+        url += "+AND+";
+    }
+
+    if (serious != null){url += `serious:${serious}`;}
+
+    if (
+        (receivedDate && reportBy)||
+        ( genericDrugName.length > 0 && reportBy) ||
+        (establishedPharmClass.length > 0 && reportBy) ||
+        (chemicalStructureClass.length > 0 && reportBy) ||
+        (chemicalStructureClass.length > 0 && reportBy) ||
+        (country.length > 0 && reportBy) ||
+        (sex != null && reportBy) ||
+        (ageGroup != null && reportBy) ||
+        (indication.length > 0 && reportBy) ||
+        (ageBegin != null && reportBy) ||
+        (serious != null && reportBy)
+    ) {
+        url += "+AND+";
+    }
+
+    if (reportBy != null){url += `${reportBy}`;}
 
 
     if (byCount) {
@@ -393,6 +633,26 @@ function constructUrl() {
             case "safetyReportId":
                 url += "safetyreportid.exact";
                 break;
+            case "sex":
+                url += "patient.patientsex";
+                break;
+            case "ageGroup":
+                url += "patient.patientagegroup";
+                break;
+            case "indication":
+                url += "patient.drug.drugindication.exact";
+                break;
+
+            case "ageRange":
+                url += "patient.patientonsetage";
+                break;
+            case "serious":
+                url += "serious";
+                break;
+            case "reportBy":
+                url += "companynumb.exact";
+                break;
+
             default:
                 break;
         }
@@ -437,6 +697,13 @@ function processCountryData(data) {
 
 // Function to process drug reaction data
 function processReactionData(data) {
+    return data.map((item) => ({
+        name: item.term,
+        count: item.count,
+    }));
+}
+
+function processIndicationData(data) {
     return data.map((item) => ({
         name: item.term,
         count: item.count,
@@ -502,6 +769,7 @@ const loadCharacters = async () => {
 loadCharacters();
 
 async function fetchDataAndPlot(url, encodedApiKey) {
+    let plotData;
     try {
         const res = await fetch(url, {
             headers: {
@@ -520,35 +788,128 @@ async function fetchDataAndPlot(url, encodedApiKey) {
             throw new Error("Invalid data format from API. Expected 'results' array.");
         }
 
-        const plotData = data.results.map(item => ({ x: item.term, y: item.count })); // Adapt to your data structure
-        if (!plotData.every(item => typeof item.x === 'string' && typeof item.y === 'number')){
-            throw new Error("Data must contain numeric x and y values for plotting")
+        let termNameMap = {};
+        if (url.includes('&count=serious')) {
+            termNameMap = {
+                1: "The adverse event resulted in death, a life threatening condition, hospitalization, disability, congenital anomaly, or other serious condition",
+                2: "The adverse event did not result in any of the above"
+            };
+        } else if (url.includes('&count=patient.patientagegroup')) {
+            termNameMap = {
+                1: "neonate",
+                2: "infant",
+                3: "child",
+                4: "adolescent",
+                5: "adult",
+                6: "elderly"
+            };
+        } else if (url.includes('&count=patient.patientsex')) {
+            termNameMap = {
+                0: "other",
+                1: "male",
+                2: "female"
+            };
         }
 
+if(url.includes('&count=receivedate')){
+    plotData = data.results.map(item => ({x: item.time.replace(/(\d{4})(\d{2})(\d{2})/g, '$1-$2-$3'), y: item.count})); // Adapt to your data structure
 
-        const trace = {
-            x: plotData.map(item => item.x),
-            y: plotData.map(item => item.y),
-            mode: 'lines',
-            type: 'line',
+}
+else {
+    plotData = data.results.map(item => ({x: termNameMap[item.term] || String(item.term), y: item.count})); // Adapt to your data structure
+    if (!plotData.every(item => typeof item.x === 'string' && typeof item.y === 'number')) {
+        throw new Error("Data must contain string x and numeric y values for plotting")
+    }
+}
 
-        };
+        if ((url.includes('&count=serious')) || (url.includes('&count=patient.patientsex')) || (url.includes('&count=patient.patientagegroup'))) {
+            const trace = {
+                labels: plotData.map(item => item.x),
+                values: plotData.map(item => item.y),
 
-        const layout = {
-            title: 'Open FDA Drug Adverse Event API',
-            margin:{
-                b:500,
-            },
-            height: 1200,
-        };
+                type: 'pie',
 
-        const config = {
-            showLink: true,
-            plotlyServerURL: "https://chart-studio.plotly.com",
-            linkText: 'Modify graph in plotly'
-        };
+            };
 
-        Plotly.newPlot('showGraph', [trace], layout,config, {scrollZoom: true}, {editable: true}, {responsive: true});
+            const layout = {
+                title: 'Open FDA Drug Adverse Event API',
+                margin: {
+                    b: 500,
+                },
+                height: 1200,
+            };
+
+            const config = {
+                showLink: true,
+                plotlyServerURL: "https://chart-studio.plotly.com",
+                linkText: 'Modify graph in plotly'
+            };
+
+            Plotly.newPlot('showGraph', [trace], layout, config, {scrollZoom: true}, {editable: true}, {responsive: true});
+        } else if ((url.includes('&count=patient.patientonsetage')) || (url.includes('&count=receivedate'))) {
+            const trace = {
+                x: plotData.map(item => item.x),
+                y: plotData.map(item => item.y),
+
+                type: 'bar',
+
+            };
+
+            const layout = {
+                title: 'Open FDA Drug Adverse Event API',
+                margin: {
+                    b: 500,
+                },
+                height: 1200,
+            };
+
+            const config = {
+                showLink: true,
+                plotlyServerURL: "https://chart-studio.plotly.com",
+                linkText: 'Modify graph in plotly'
+            };
+
+            Plotly.newPlot('showGraph', [trace], layout, config, {scrollZoom: true}, {editable: true}, {responsive: true});
+        }
+        else if (url.includes('&count=safetyreportid.exact')) {
+            document.getElementById('showGraph').innerHTML =
+                "<p>" + "Count of Safety Report IDs Provided: " + plotData.length + "</p>";
+
+            let output = "";
+
+            for (let i = 0; i < plotData.length; i++) {
+                let http = `https://api.fda.gov/drug/event.json?search=safetyreportid:${plotData[i].x}`; // Template literal for URL
+                output += `<a class="label" target="_blank" href="${http}">ID Report: ${plotData[i].x} Count: ${plotData[i].y}</a>`; // Template literal for HTML
+            }
+
+            document.getElementById('showGraph').innerHTML += output;
+
+        }
+        else {
+            const trace = {
+                x: plotData.map(item => item.x),
+                y: plotData.map(item => item.y),
+                mode: 'lines',
+                type: 'line',
+
+            };
+
+            const layout = {
+                title: 'Open FDA Drug Adverse Event API',
+                margin: {
+                    b: 500,
+                },
+                height: 1200,
+            };
+
+            const config = {
+                showLink: true,
+                plotlyServerURL: "https://chart-studio.plotly.com",
+                linkText: 'Modify graph in plotly'
+            };
+
+            Plotly.newPlot('showGraph', [trace], layout, config, {scrollZoom: true}, {editable: true}, {responsive: true});
+        }
 
     } catch (error) {
         console.error("Error fetching or processing data:", error);
